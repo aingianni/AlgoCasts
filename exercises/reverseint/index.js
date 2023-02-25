7b4902b2ccc922c215bd8c895e1db59463bc01f9
Anthony Ingianni <ingianni.anthony@gmail.com>
2023-02-24 21:38:00 -0500

    completed reverseint problem

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,16 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    if (n > 0) {
+      return parseInt(n.toString().split('').reverse().join(''))
+    } else if (n < 0) {
+      let result = '-'
+      result += n.toString().split('').reverse().join('')
+      return parseInt(result)
+    } else {
+      return 0
+    }
+  }
 
 module.exports = reverseInt;
