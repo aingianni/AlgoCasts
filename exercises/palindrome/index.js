dfd158a869bf7563c8aeb3b1f1c0727a120dc4dc
Anthony Ingianni <ingianni.anthony@gmail.com>
2023-02-25 08:46:09 -0500

    completed palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,13 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    let result = ''
+    for (let i = str.length-1; i >= 0; i--) {
+        result += str[i]
+    }
+    
+    return result === str
+}
 
 module.exports = palindrome;
