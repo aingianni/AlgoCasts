056620529486b75be17bb7e2bfd206d8956be7b1
Anthony Ingianni <ingianni.anthony@gmail.com>
2023-03-11 08:52:06 -0500

    completed vowels problem

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,17 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    let result = 0
+    let vowels = 'aeiou'
+    for (let i = 0; i < str.length; i++) {
+        for (let k = 0; k < vowels.length; k++) {
+            if (vowels[k] === str[i].toLowerCase()) {
+                result++
+            }
+        }
+    }
+    return result
+}
 
 module.exports = vowels;
